0acbe91a674e5c320a12abe6e9461272e55eacf6
Michael Kashirin <m.k4shirin@yandex.ru>
2024-12-09 18:44:44 +0300

    Add Limited Pythoin C API

--- src/c.zig
+++ src/c.zig
@@ -1,5 +1,6 @@
 // Export the Python C API
 pub usingnamespace @cImport({
+    @cDefine("Py_LIMITED_API", "0x030B0000"); // Hardcoded for 3.11 for now
     @cDefine("PY_SSIZE_T_CLEAN", {});
     @cInclude("Python.h");
     @cInclude("structmember.h");
