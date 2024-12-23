6e06bb52fb3f26824b77c2bef28859467cd2525d
Michael Kashirin <m.k4shirin@yandex.ru>
2024-12-23 08:41:28 +0300

    Remove c.zig

--- src/c.zig
+++
@@ -1,6 +0,0 @@
-pub usingnamespace @cImport({
-    @cDefine("Py_LIMITED_API", "0x030b0000");
-    @cDefine("PY_SSIZE_T_CLEAN", {});
-    @cInclude("Python.h");
-    @cInclude("structmember.h");
-});
